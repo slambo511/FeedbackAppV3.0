933b5f0338026990f6e27a61927645944fba0ce2
Samuel Bancroft <samuelbancroft@hotmail.com>
2018-12-21 19:44:11 +0000

    test

--- FeedbackAppV3.0/Form1.cs
+++ FeedbackAppV3.0/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AESEncryptor;
 
 namespace FeedbackAppV3._0
 {
@@ -15,6 +17,20 @@ namespace FeedbackAppV3._0
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var original = "Here is some data to encrypt";
+            using (var myRijndael = new RijndaelManaged())
+            {
+                myRijndael.GenerateKey();
+                myRijndael.GenerateIV();
+                var enc = new Encryptor();
+                byte[] encrypted = Encryptor(original, myRijndael.Key, myRijndael.IV);
+
+
+            }
         }
     }
 }
